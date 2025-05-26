332ed6a870fd8bf547c51b90a1213b3cd520dbdc
Mike O'Connor <mike@haven2.com>
2025-05-26 09:53:38 -0500

    Add files via upload

---
+++ BASH Scripts/maintenance_PGBWB_nightly.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+# PGBEB writes temporary files to this directory when users request audio for an ID
+# this nightly script empties the folder they're written to.
+
+rm -rf /Users/mikeoconnor/Documents/BirdWorkbench/BirdWorkbench_R_Scripts/www/*
