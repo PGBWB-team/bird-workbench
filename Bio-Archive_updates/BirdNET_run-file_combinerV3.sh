c1a80abbdfad2089a6b2828ff975b26e8f140e2e
Mike O'Connor <mike@haven2.com>
2025-07-09 11:03:00 -0500

    Update BirdNET_run-file_combinerV3.sh
    Adding comments and making style consistent

--- Bio-Archive_updates/BirdNET_run-file_combinerV3.sh
+++ Bio-Archive_updates/BirdNET_run-file_combinerV3.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+# This script is typically called as a part of the Archive to FST Update process
+# after BirdNET-Analyzer has created new results files that need to be merged into
+# a single results file for the year.  It deletes the old combined-results file,
+# loops through the directory containing BirdNET's results files for the year and merges all the
+# results into a single file for the year
+
 # EDIT - Directory containing the files to process
 input_dir="/Users/mikeoconnor/Documents/BirdWorkbench/birdnet_analyzer_RUN_FILES/run_files_2025" 
 # EDIT - Output file
