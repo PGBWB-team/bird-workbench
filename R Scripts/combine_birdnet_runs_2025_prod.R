d3d592d08acc1699149e075bc277cb89973b3a9b
Mike O'Connor <mike@haven2.com>
2025-08-18 10:52:52 -0500

    Update combine_birdnet_runs_2025_prod.R

--- R Scripts/combine_birdnet_runs_2025_prod.R
+++ R Scripts/combine_birdnet_runs_2025_prod.R
@@ -1,4 +1,15 @@
-# This file will used to combine BirdNET-Analyzer results 
+# This script:
+# 
+# Is downstream from the BirdNET_run_file_combiner_prod.sh BASH script,
+# Reads a single-year text file of BirdNET IDs, 
+# Cleans up the data, 
+# Uses fuzzy logic to join each row with averages calculated from SQL-based local weather 
+#    observations, 
+# Creates a single-year FST file, and 
+# Combines it with other such files to create an all-years FST that's used as input by 
+#    the UI script.
+
+
 
 library(lubridate)
 library(fst)
@@ -13,7 +24,7 @@ library(fuzzyjoin)
 #################################################
 
 # This text file contains the BirdNET-Analyzer results for a year
-## Point to the current-year version of that file *** #
+## *** Point to the current-year version of that file *** #
 
 input_COMBINED_file <- "/Users/mikeoconnor/Documents/BirdWorkbench/birdnet_analyzer_run_files_COMBINED/run_files_COMBINED_2025.txt"
 
